1a296f84cdd224601c918cf0a43d2bd43be15bb4
Hauke D <haukex@zero-g.net>
2018-11-10 09:38:05 +0100

    Added Kwalitee badge generation

--- misc/cpantesters.pl
+++ misc/cpantesters.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use re '/aa';
 use Getopt::Std 'getopts';
 use URI ();
 use HTTP::Tiny ();
@@ -9,18 +10,23 @@ use File::Spec::Functions qw/curdir catfile/;
 use Badge::Simple qw/badge/;
 use File::Replace 'replace3';
 
-# Generate "CPAN Testers" badges for a CPAN author's modules
+# Generate "CPAN Testers" (and "Kwalitee") badges for a CPAN author's modules
 
-my $USAGE = "Usage: $0 [-vdq] [-o OUTDIR] CPANAUTHOR\n";
+my $USAGE = "Usage: $0 [-vdq] [-o OUTDIR] [-k KWALDIR] CPANAUTHOR\n";
 $Getopt::Std::STANDARD_HELP_VERSION=1;
-getopts('vdqo:', \my %opts) or die $USAGE;
+getopts('vdqo:k:', \my %opts) or die $USAGE;
 my $VERBOSE = !!$opts{v};
 my $DEBUG = !!$opts{d};
 my $QUIET = !!$opts{q};
 my $OUTDIR = defined($opts{o}) ? $opts{o} : curdir;
 die "not a directory: $OUTDIR" unless -d $OUTDIR;
+my $KWALDIR = $opts{k};
+die "not a directory: $KWALDIR" if defined($KWALDIR) && !-d $KWALDIR;
+warn "Warning: OUTDIR and KWALDIR shouldn't be the same\n"
+	if defined($KWALDIR) && $KWALDIR eq $OUTDIR;
 @ARGV==1 or die $USAGE;
 my $AUTHOR = uc $ARGV[0];
+die "bad author $AUTHOR" unless $AUTHOR=~/\A\w+\z/;
 
 if ($DEBUG) { require Data::Dump; Data::Dump->import("dd") }
 
@@ -28,7 +34,6 @@ my $http = HTTP::Tiny->new();
 
 my @dists = do {
 	my $uri = URI->new('http://fastapi.metacpan.org/v1/release/_search');
-	die "bad author $AUTHOR" unless $AUTHOR=~/\A\w+\z/;
 	$uri->query_form({ q=>"author:$AUTHOR AND status:latest",
 		fields=>"distribution,version" });
 	my $resp = $http->get("$uri");
@@ -43,8 +48,8 @@ my @dists = do {
 die "Sorry, no hits for author $AUTHOR\n" unless @dists;
 
 for my $dist (@dists) {
-	$$dist[0]=~/\A[\w\-]+\z/aa or die "bad dist: $$dist[0]";
-	$$dist[1]=~/\A[\w\-\.]+\z/aa or die "bad version: $$dist[1]";
+	$$dist[0]=~/\A[\w\-]+\z/ or die "bad dist: $$dist[0]";
+	$$dist[1]=~/\A[\w\-\.]+\z/ or die "bad version: $$dist[1]";
 	my $uri = URI->new('http://api.cpantesters.org/v3/release/dist');
 	$uri->path_segments( $uri->path_segments, $$dist[0], $$dist[1] );
 	my $resp = $http->get("$uri");
@@ -72,5 +77,34 @@ for my $dist (@dists) {
 		->toString;
 	$repl->finish;
 	print STDERR "Wrote $outfile ($text $color)\n" unless $QUIET;
+	print STDERR "Suggested link: http://matrix.cpantesters.org/?dist=$$data{dist}\n" unless $QUIET;
+}
+
+if (defined $KWALDIR) {
+	my $uri = "https://cpants.cpanauthors.org/author/HAUKEX.json";
+	my $resp = $http->get("$uri");
+	die "$uri: $$resp{status} $$resp{reason}" unless $resp->{success};
+	print STDERR "$uri: $$resp{status} $$resp{reason}\n" if $VERBOSE;
+	my $data = decode_json($resp->{content});
+	$DEBUG and dd($data);
+	for my $kd ( @{ $data->{cpan_distributions} } ) {
+		my ($name,$score) = @$kd{'name','score'};
+		$name=~/\A[\w\-]+\z/ or die "bad dist: $name";
+		$score=~/\A\d+(?:\.\d+)?\z/ or die "bad score: $score";
+		my $color;
+		# looking at https://cpants.cpanauthors.org/ranking,
+		# it seems we should probably set the cutoffs pretty high
+		if    ($score>=100) { $color="brightgreen" }
+		elsif ($score>=95)  { $color="green"       }
+		elsif ($score>=80)  { $color="yellow"      }
+		else                { $color="red"         }
+		my $outfile = catfile($KWALDIR,"$name.svg");
+		my (undef,$outfh,$repl) = replace3($outfile, perms=>0644, debug=>$DEBUG);
+		print {$outfh} badge( left=>"kwalitee", right=>$score, color=>$color )
+			->toString;
+		$repl->finish;
+		print STDERR "Wrote $outfile ($score $color)\n" unless $QUIET;
+		print STDERR "Suggested link: https://cpants.cpanauthors.org/dist/$name\n" unless $QUIET;
+	}
 }
 
