9f521f5df6292071997b0200cd081271f3f1ffb1
Hauke D <haukex@zero-g.net>
2024-04-27 14:04:50 +0200

    Added HTML generation

--- misc/cpantesters.pl
+++ misc/cpantesters.pl
@@ -6,15 +6,15 @@ use Getopt::Std 'getopts';
 use URI ();
 use HTTP::Tiny ();
 use JSON::MaybeXS qw/decode_json/;
-use File::Spec::Functions qw/curdir catfile/;
+use File::Spec::Functions qw/curdir catfile splitdir/;
 use Badge::Simple qw/badge/;
 use File::Replace 'replace3';
 
 # Generate "CPAN Testers" (and "Kwalitee") badges for a CPAN author's modules
 
-my $USAGE = "Usage: $0 [-vdq] [-o OUTDIR] [-k KWALDIR] CPANAUTHOR\n";
+my $USAGE = "Usage: $0 [-vdq] [-o OUTDIR] [-k KWALDIR] [-h HTMLOUT] CPANAUTHOR\n";
 $Getopt::Std::STANDARD_HELP_VERSION=1;
-getopts('vdqo:k:', \my %opts) or die $USAGE;
+getopts('vdqo:k:h:', \my %opts) or die $USAGE;
 my $VERBOSE = !!$opts{v};
 my $DEBUG = !!$opts{d};
 my $QUIET = !!$opts{q};
@@ -24,6 +24,13 @@ my $KWALDIR = $opts{k};
 die "not a directory: $KWALDIR" if defined($KWALDIR) && !-d $KWALDIR;
 warn "Warning: OUTDIR and KWALDIR shouldn't be the same\n"
 	if defined($KWALDIR) && $KWALDIR eq $OUTDIR;
+my $HTMLOUT;
+if ($opts{h}) {
+	die "please specify a filename without path for HTMLOUT"
+		if @{[splitdir $opts{h}]}!=1;
+	$HTMLOUT = catfile($OUTDIR,$opts{h});
+	die "not a file: $HTMLOUT" if -e $HTMLOUT && !-f $HTMLOUT;
+}
 @ARGV==1 or die $USAGE;
 my $AUTHOR = uc $ARGV[0];
 die "bad author $AUTHOR" unless $AUTHOR=~/\A\w+\z/;
@@ -47,6 +54,23 @@ my @dists = do {
 		@{$data->{hits}{hits}};
 };
 die "Sorry, no hits for author $AUTHOR\n" unless @dists;
+print STDERR "Found ",0+@dists," dists for $AUTHOR\n" unless $QUIET;
+
+my $htmlo;
+if ($HTMLOUT) {
+	$htmlo = File::Replace->new($HTMLOUT, ':raw:encoding(UTF-8)', perms=>0644, debug=>$DEBUG);
+	print {$htmlo->out_fh} <<"END HTML";
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>CPAN Testers Badges for $AUTHOR</title>
+</head>
+<body>
+<h1>CPAN Testers Badges for $AUTHOR</h1>
+<ul>
+END HTML
+}
 
 for my $dist (@dists) {
 	$$dist[0]=~/\A[\w\-]+\z/ or die "bad dist: $$dist[0]";
@@ -81,13 +105,27 @@ for my $dist (@dists) {
 		elsif ($percent>=50)  { $color="yellow"      }
 		else                  { $color="red"         }
 	}
-	my $outfile = catfile($OUTDIR,"$$data{dist}.svg");
+	my $basename = "$$data{dist}.svg";
+	my $outfile = catfile($OUTDIR,$basename);
 	my (undef,$outfh,$repl) = replace3($outfile, perms=>0644, debug=>$DEBUG);
 	print {$outfh} badge( left=>"CPAN Testers", right=>$text, color=>$color )
 		->toString;
 	$repl->finish;
 	print STDERR "Wrote $outfile ($text $color)\n" unless $QUIET;
 	print STDERR "Suggested link: http://matrix.cpantesters.org/?dist=$$data{dist}\n" unless $QUIET;
+	print {$htmlo->out_fh}
+		qq{\t<li>$$data{dist}: <a href="http://matrix.cpantesters.org/?dist=$$data{dist}">}
+		.qq{<img src="$basename" alt="$$data{dist} CPAN Testers: $text"></a></li>\n} if $htmlo;
+}
+
+if ($htmlo) {
+	print {$htmlo->out_fh} <<"END HTML";
+</ul>
+</body>
+</html>
+END HTML
+	$htmlo->finish;
+	print STDERR "Wrote $HTMLOUT\n" unless $QUIET;
 }
 
 if (defined $KWALDIR) {
