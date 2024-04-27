100a787a23817694dabafd9fdee180d93e0386f7
Hauke D <haukex@zero-g.net>
2024-04-27 14:06:28 +0200

    Removed deprecated Kwalitee badge generation

--- misc/cpantesters.pl
+++ misc/cpantesters.pl
@@ -10,9 +10,9 @@ use File::Spec::Functions qw/curdir catfile splitdir/;
 use Badge::Simple qw/badge/;
 use File::Replace 'replace3';
 
-# Generate "CPAN Testers" (and "Kwalitee") badges for a CPAN author's modules
+# Generate "CPAN Testers" badges for a CPAN author's modules
 
-my $USAGE = "Usage: $0 [-vdq] [-o OUTDIR] [-k KWALDIR] [-h HTMLOUT] CPANAUTHOR\n";
+my $USAGE = "Usage: $0 [-vdq] [-o OUTDIR] [-h HTMLOUT] CPANAUTHOR\n";
 $Getopt::Std::STANDARD_HELP_VERSION=1;
 getopts('vdqo:k:h:', \my %opts) or die $USAGE;
 my $VERBOSE = !!$opts{v};
@@ -20,10 +20,8 @@ my $DEBUG = !!$opts{d};
 my $QUIET = !!$opts{q};
 my $OUTDIR = defined($opts{o}) ? $opts{o} : curdir;
 die "not a directory: $OUTDIR" unless -d $OUTDIR;
-my $KWALDIR = $opts{k};
-die "not a directory: $KWALDIR" if defined($KWALDIR) && !-d $KWALDIR;
-warn "Warning: OUTDIR and KWALDIR shouldn't be the same\n"
-	if defined($KWALDIR) && $KWALDIR eq $OUTDIR;
+die "Kwalitee badge generation removed, please use the native service at\n"
+	."https://cpants.cpanauthors.org/dist/Dist-Name.svg\n" if $opts{k};
 my $HTMLOUT;
 if ($opts{h}) {
 	die "please specify a filename without path for HTMLOUT"
@@ -127,34 +125,3 @@ END HTML
 	$htmlo->finish;
 	print STDERR "Wrote $HTMLOUT\n" unless $QUIET;
 }
-
-if (defined $KWALDIR) {
-	warn "Kwalitee SVG badges are now provided natively: https://cpants.cpanauthors.org/dist/Dist-Name.svg\n";
-	my $uri = "https://cpants.cpanauthors.org/author/HAUKEX.json";
-	my $resp = $http->get("$uri");
-	$$resp{success} or die "$uri: $$resp{status} "
-		.( $$resp{status}==599 ? $$resp{content} : $$resp{reason} );
-	print STDERR "$uri: $$resp{status} $$resp{reason}\n" if $VERBOSE;
-	my $data = decode_json($resp->{content});
-	$DEBUG and dd($data);
-	for my $kd ( @{ $data->{cpan_distributions} } ) {
-		my ($name,$score) = @$kd{'name','score'};
-		$name=~/\A[\w\-]+\z/ or die "bad dist: $name";
-		$score=~/\A\d+(?:\.\d+)?\z/ or die "bad score: $score";
-		my $color;
-		# looking at https://cpants.cpanauthors.org/ranking,
-		# it seems we should probably set the cutoffs pretty high
-		if    ($score>=100) { $color="brightgreen" }
-		elsif ($score>=95)  { $color="green"       }
-		elsif ($score>=80)  { $color="yellow"      }
-		else                { $color="red"         }
-		my $outfile = catfile($KWALDIR,"$name.svg");
-		my (undef,$outfh,$repl) = replace3($outfile, perms=>0644, debug=>$DEBUG);
-		print {$outfh} badge( left=>"kwalitee", right=>$score, color=>$color )
-			->toString;
-		$repl->finish;
-		print STDERR "Wrote $outfile ($score $color)\n" unless $QUIET;
-		print STDERR "Suggested link: https://cpants.cpanauthors.org/dist/$name\n" unless $QUIET;
-	}
-}
-
